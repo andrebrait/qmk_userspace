87d9866ad799f23a1ce9a6800836c3662af1041d
Andre Brait <andrebrait@gmail.com>
2024-02-21 09:50:22 +0100

    Build against upstream develop branch, small improvements

--- keyboards/gmmk/pro/rev1/ansi/keymaps/andrebrait/rules.mk
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/andrebrait/rules.mk
@@ -6,12 +6,12 @@ MOUSEKEY_ENABLE = no
 # Use sym_eager_pk for better latency when gaming.
 #
 # Previous issues with sym_defer_pk (and sym_eager_pk and debounce times
-# shorter than 8ms) were due to Glorious' horrible hot-swap sockets. 
+# shorter than 8ms) were due to Glorious' horrible hot-swap sockets.
 # These issues were 100% resolved after I replaced them with Gateron hot-swap sockets.
 #
 # The old sockets apparently didn't hold onto the legs of the switches at all.
 #
-# Using eager might not be a good idea with other switch types, but I only use 
+# Using eager might not be a good idea with other switch types, but I only use
 # linears now, so this should be ok.
 #
 # asym_eager_defer_pk also works well and it is more resistant to chatter at
